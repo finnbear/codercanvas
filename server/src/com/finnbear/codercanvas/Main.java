6f76163c5a39ab76548c9a6ec4c25814e270052d
finnbear <finnbearone@gmail.com>
2017-04-23 20:50:05 -0700

    Added additional test to driver class.

--- server/src/com/finnbear/codercanvas/Main.java
+++ server/src/com/finnbear/codercanvas/Main.java
@@ -8,10 +8,16 @@ public class Main {
     public static void main(String[] args) {
         FileSystemManager fileSystemManager = new FileSystemManager();
 
-        SharedBitmap sharedBitmap = new SharedBitmap(10, 10);
+        SharedBitmap sharedBitmap1 = new SharedBitmap(10, 10);
 
-        sharedBitmap.setPixel(5, 5, 255, 0);
+        sharedBitmap1.setPixel(5, 5, 255, 0);
 
-        sharedBitmap.saveBitmap(fileSystemManager, "bitmap1");
+        sharedBitmap1.saveBitmap(fileSystemManager, "bitmap1");
+
+        SharedBitmap sharedBitmap2 = new SharedBitmap(10, 10);
+
+        sharedBitmap2.loadBitmap(fileSystemManager, "bitmap1");
+
+        System.out.println(sharedBitmap2.getPixel(5, 5)); // Output: 255
     }
 }
